2ba5829e409d23baebda81ef6979dae5d6866dc7
Peremyshlennikova Natalia <natalia47256@gmail.com>
2024-12-12 21:31:14 +0100

    Update fullscript.sh
    Changes

--- fullscript.sh
+++ fullscript.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
-echo "Hello Student!"
-echo "Enter your name"
-read NAME
-echo "Welcome to terminal $NAME"
-mkdir /tmp/test
-touch /tmp/test
-date +"%H:%M:%S" >> mydate.txt
-echo "Daten werden gespeichert"
-for run in {1..10}
-do
-sleep 0.5
-echo $run
-done
-echo "Data saved. continue work"
-df - h >> mydate.txt
-
+  1 #!/bin/bash
+  2 echo "Hello Student!"
+  3 echo "Введите, пожалуйста, Ваше имя: "
+  4 read NAME
+  5 echo "Welcome to terminal $name"
+  6 mkdir /tmp/test
+  7 touch /tmp/mydate.txt
+  8 date +"%H:%M:$S" >> mydate.txt
+  9 echo "Сохраняем данные"
+ 10 for run in {1..10}
+ 11 do
+ 12 echo $run
+ 13 sleep 0.5
+ 14 done
+ 15 echo "Data saved.continue work"
+ 16 df -h >> /tmp/mydate.txt
+ 17 cp /tmp/mydate.txt /opt/mydate/newmydate.txt
+ 18 for run in {1..5}
+ 19 do
+ 20 echo $run
+ 21 sleep 1
+ 22 done
+ 23 echo "Well done Boss"
